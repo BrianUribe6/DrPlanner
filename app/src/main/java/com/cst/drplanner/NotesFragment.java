4ca4118ff606d12b4ff833687357a42abbd90241
Chris <christianrod97@gmail.com>
2019-04-29 14:40:39 -0400

    Working on recycler view, not fully added.

--- app/src/main/java/com/cst/drplanner/NotesFragment.java
+++ app/src/main/java/com/cst/drplanner/NotesFragment.java
@@ -3,16 +3,37 @@ package com.cst.drplanner;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class NotesFragment extends Fragment {
+
+    private RecyclerView.LayoutManager layoutManager;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_notes, container, false);
+        View view = inflater.inflate(R.layout.fragment_notes, container, false);
+
+
+        //Floating Action Button in fragment notes
+        FloatingActionButton fabtnCreateNote = view.findViewById(R.id.fabtnCreateNote);
+
+        //Recycler view in fragment notes
+        RecyclerView rviewNotes = view.findViewById(R.id.rviewNotes);
+        rviewNotes.setHasFixedSize(true);
+
+     //   layoutManager = new LinearLayout();
+        rviewNotes.setLayoutManager(layoutManager);
+
+        return view;
     }
+
+
+
 }
